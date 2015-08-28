b89615ae5e75c79c4bd5aa3efd1d096fbeb5abdb
FrackenToaster <frakentoaster@gmail.com>
2015-08-27 22:45:37 -0700

    Added valid attributes to people controller spec

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe PeopleController, type: :controller do
   # Person. As you add validations to Person, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { first_name: 'Leslie', last_name: 'Knope' }
   }
 
   let(:invalid_attributes) {
